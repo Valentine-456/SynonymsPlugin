12692f9fd7d1f8369dd553dc6761b57b7d990b4f
Valentyn Bondarenko <murica.it@gmail.com>
2023-12-28 23:54:35 +0100

    Refactor ChangeTextIntentionAction (reduce amount of errors and warning)

--- src/main/kotlin/com/github/valentine456/synonymsplugin/intentions/ChangeTextIntentionAction.kt
+++ src/main/kotlin/com/github/valentine456/synonymsplugin/intentions/ChangeTextIntentionAction.kt
@@ -1,11 +1,10 @@
 package com.github.valentine456.synonymsplugin.intentions
 
-import com.github.valentine456.synonymsplugin.services.*
+import com.github.valentine456.synonymsplugin.services.setUpThesaurusService
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.SelectionModel
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.psi.PsiFile
@@ -30,13 +29,18 @@ class ChangeTextIntentionAction : IntentionAction {
     }
 
     override fun invoke(project: Project, editor: Editor, file: PsiFile?) {
-        val document = editor.document
-        val selectionModel = editor.selectionModel
         val selectedText = editor.selectionModel.selectedText ?: return
 
-        val myService = setUpThesaurusService(project)
+        val myService = setUpThesaurusService(editor.project!!)
         val identifier = selectedText.lowercase()
         val synonyms = arrayListOf(identifier) + myService.getSynonyms(identifier)
+
+        ApplicationManager.getApplication().invokeLater {
+            checkAndPerformAction(editor, project, synonyms)
+        }
+    }
+
+    private fun checkAndPerformAction(editor: Editor, project: Project, synonyms: List<String>) {
         val chosenItem = Messages.showEditableChooseDialog(
             "Select an option:",
             "Choose Option",
@@ -45,25 +49,17 @@ class ChangeTextIntentionAction : IntentionAction {
             synonyms.first(),
             null
         )
+
         chosenItem?.let {
-            editor.document.replaceString(selectionModel.selectionStart, selectionModel.selectionEnd, chosenItem)
+            WriteCommandAction.runWriteCommandAction(project) {
+                editor.document.replaceString(
+                    editor.selectionModel.selectionStart, editor.selectionModel.selectionEnd, chosenItem
+                )
+            }
         }
     }
 
     override fun startInWriteAction(): Boolean {
         return true // Set to true if your action requires write access to PSI elements
-    }
-
-    private fun setUpThesaurusService(project: Project): ThesaurusService {
-        val myService: ThesaurusService = ServiceManager.getService(project, FileThesaurusService::class.java)
-        val myService2: ThesaurusService = ServiceManager.getService(project, BigHugeThesaurusService::class.java)
-        val myService3: ThesaurusService = ServiceManager.getService(project, ApiNinjaThesaurusService::class.java)
-        val myService4: ThesaurusService = ServiceManager.getService(project, MockThesaurusService::class.java)
-
-        myService.nextService = myService2
-        myService2.nextService = myService3
-        myService3.nextService = myService4
-
-        return myService
     }
 }
